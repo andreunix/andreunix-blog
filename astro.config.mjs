a869e6812e3fbdb2e67135c60db84997dcae386d
andreunix <andreunix@protonmail.com>
2024-10-16 13:51:09 -0400

    andreunix blog

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://andreunix.com",
+  integrations: [mdx(), sitemap(), solidJs(), tailwind({
+    applyBaseStyles: false
+  })]
+});
